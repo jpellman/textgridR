8db0211a11641a6fde06b5afeb510378c243f473
jaipel <pellman.john@gmail.com>
2013-09-11 19:41:25 -0400

    read.TextGrid is debugged and decently functional (until it's not, at least).  Making this commit before the power goes out.  I'm starin' at you, nebulous sky above, rife with electrical spike candy and drunken Greek gods (that's my way of saying that I'm in the middle of a particularly nasty thunderstorm).

--- R/read.TextGrid.R
+++ R/read.TextGrid.R
@@ -29,53 +29,60 @@ read.TextGrid <- function(file) {
     on.exit(close(file))
   }
   # Uses the readLiness function to determine if "File type = "ooTextFile"" is the first line; if not, throws an error, telling the user that this is not a TextGrid.
-  if (!readLines(file)!='File type = "ooTextFile"'){
+  if (readLines(file, 1)!='File type = "ooTextFile"'){
     stop("File is not a TextGrid of type 'ooTextFile'.")
   }
   # Uses the readLiness function to determine if "Object class = "TextGrid"" is the second line; if not throws an error, telling the user that this is not a TextGrid and that, while sad, life will go on without all of the TextGriddy goodness.
-  if (!readLines(file)!='Object class = "TextGrid"'){
+  if (readLines(file, 2)[1]!='Object class = "TextGrid"'){
     stop("File is not a TextGrid.")
   }
-  # Skips one line with readLines.
-  readLines(file)
+  # Skips one line with readLines. Done implicitly in above if statement.
   # The above could probably be more concisely expressed with one readLiness statement skipping 3 lines, and the lines being assigned to a temp var to check if the file type is right.
   # Have this function parse and evaluate the next two lines to obtain xmin and xmax.  Store in appropriately named variables.  At the end of the function, these variables will be returned in a list.
-  xminTextGrid <- eval(parse(readLines(file)))
-  xmaxTextGrid <- eval(parse(readLines(file)))
+  xminTextGrid <- unlist(strsplit(readLines(file, 1), " "))
+  xminTextGrid <- as.double(xminTextGrid[length(xminTextGrid)])
+  xmaxTextGrid <- unlist(strsplit(readLines(file, 1), " "))
+  xmaxTextGrid <- as.double(xmaxTextGrid[length(xmaxTextGrid)])
   # Read in the next line.  If tiers do exist, indicate as such in a logical aptly named "tiers"
-  if(readLines(file)=='tiers? <exists> '){
+  if(readLines(file, 1)=='tiers? <exists> '){
     tiers <- TRUE
   } else {
     tiers <- FALSE
   }
-  sizeTextGrid <- eval(parse(readLines(file)))
+  sizeTextGrid <- unlist(strsplit(readLines(file, 1), " "))
+  sizeTextGrid <- as.double(sizeTextGrid[length(sizeTextGrid)])
   # Skip over the TextGrid declaration of an array "item" (also perhaps check to see if this declaration did, in fact, occur). Initialize "item" as a list (or perhaps a vector if possible and we want to save RAM; or perhaps a multi-way array).
-  if(readLines(file)=='item []: '){
+  if(readLines(file, 1)=='item []: '){
     item <- list()
   } else {
     stop("Error parsing TextGrid array.")
   }
   # While the length of item index is not equal to sizeTextGrid, read in IntervalTiers and store them in the R variable "item".
   while (length(item)!=sizeTextGrid){
-    itemIndex <- grep('[1234567890]', readLines(file))
-    if (readLines(file)=='class = "IntervalTier" '){
-      tierName <- eval(parse(readLines(file)))
-      tierxmin <- eval(parse(readLines(file)))
-      tierxmax <- eval(parse(readLines(file)))
-      tierSize <- grep('[1234567890]', readLines(file))
-      rm(name, xmin, xmax)
+    itemIndex <- gsub("[^1234567890]", "", readLines(file, 1))  
+    if (readLines(file, 1)=='        class = "IntervalTier" '){
+      tierName <- unlist(strsplit(readLines(file, 1), " "))
+      tierName <- as.character(tierName[length(tierName)])
+      tierxmin <- unlist(strsplit(readLines(file, 1), " "))
+      tierxmin <- as.double(tierxmin[length(tierxmin)])
+      tierxmax <- unlist(strsplit(readLines(file, 1), " "))
+      tierxmax <- as.double(tierxmax[length(tierxmax)])
+      tierSize <- gsub("[^1234567890]", "", readLines(file, 1))
       tempTierIntervals <- list()
       for (i in 1:tierSize){
-        intervalIndex <- grep('[1234567890]', readLines(file))
-        eval(parse(readLines(file)))
-        eval(parse(readLines(file)))
-        eval(parse(readLines(file)))
-        tempTierIntervals[intervalIndex] <- TextGrid.Interval(xmin, xmax, text)
+        intervalIndex <- gsub("[^1234567890]", "", readLines(file, 1))
+        intervalxmin <- unlist(strsplit(readLines(file, 1), " "))
+        intervalxmin <- as.double(intervalxmin[length(intervalxmin)])
+        intervalxmax <- unlist(strsplit(readLines(file, 1), " "))
+        intervalxmax <- as.double(intervalxmax[length(intervalxmax)])
+        text <- unlist(strsplit(readLines(file, 1), " "))
+        text <- as.character(text[length(text)])
+        tempTierIntervals[[intervalIndex]] <- TextGrid.Interval(intervalxmin, intervalxmax, text)
       }
     } else {
       stop("Error parsing IntervalTiers.")
     }
-    item[itemIndex] <- TextGrid.IntervalTier(tierName, tierxmin, tierxmax, tempTierIntervals)
+    item[[itemIndex]] <- TextGrid.IntervalTier(tierName, tierxmin, tierxmax, tempTierIntervals)
   }
   # To read in an IntervalTier:
     # read in the line :         class = "IntervalTier" ; check to make sure this is correct
@@ -85,6 +92,6 @@ read.TextGrid <- function(file) {
     # When the IntervalTier object is done, place it in "item" and repeat the process (i.e., the loop; although it might not be a loop if there is some way to use an apply function instead)
     # Interval objects should be created in in much the same way as IntervalTiers; using parse and eval with readLines (unless I find a better way).
   # When all the IntervalTiers have been read in and added to "item", return a structure of class "TextGrid" that contains xmin, xmax, logical tiers, the size
-  grid <- structure(item, class="TextGrid", xmin=xminTextGrid, xmax=xmaxTextGrid, tiers=tiers, size=sizeTextGrid)
-  grid
+  textgrid <- structure(item, class="TextGrid", xmin=xminTextGrid, xmax=xmaxTextGrid, tiers=tiers, size=sizeTextGrid)
+  textgrid
 }
