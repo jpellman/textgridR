17f813b7760d9b059fc0e3a97bd1d1de2080b563
jaipel <pellman.john@gmail.com>
2013-11-14 13:40:22 -0500

    A working version of write.TextGrid is finished.  Still rough, but functional.

--- R/write.TextGrid.R
+++ R/write.TextGrid.R
@@ -23,13 +23,26 @@ write.TextGrid <- function(x, file){
   tg <- c('File type = "ooTextFile"', 'Object class = "TextGrid"', '', 
           paste0('xmin = ', attributes(x)$xmin), paste0('xmax = ', attributes(x)$xmax),
           'tiers? <exists>', paste0('size = ', attributes(x)$size), 'item []:')
-  
+  # There's probably an apply-based way of doing this, but we'll stick to for loops for now.
+  for (i in 1:length(x)){
+    tg <- c(tg, paste0('    ', 'item [',i,']:'))
+    if (class(x[[i]])=="TextGrid.IntervalTier"){
+      tg <- c(tg, '        class = "IntervalTier"')
+    }
+    tg <- c(tg, paste0('        name = "', attributes(x[[i]])$name,'"'), 
+            paste0('        xmin = ', attributes(x[[i]])$xmin), paste0('        xmax = ', attributes(x[[i]])$xmax), 
+            paste0('        intervals: size = ', length(x[[i]])))
+    for (j in 1:length(x[[i]])){
+      tg <- c(tg, paste0('        intervals [',j,']:'), paste0('            xmin = ', grid[[i]][[j]]$xmin),
+              paste0('            xmax = ', grid[[i]][[j]]$xmax), paste0('            text = "', grid[[i]][[j]]$text,'"'))
+    }
+  }
   # The above is quite a bit more complicated than it seems right now, and requires
   # a well-rested brain to fully deliberate what needs to be done (i.e., I need to review
   # the formatting of a TextGrid file before I can proceed).
   
   # Pipe that character vector out to file using writeLines().
-  
+  writeLines(tg, file)
   # Reminder: write is not an S3 generic.
   
   return(TRUE)
