2c03180bf087a38f16a9389e23ecb541b70a0a0d
jaipel <pellman.john@gmail.com>
2013-12-25 18:20:47 -0500

    Tweaks to summary.TextGrid

--- R/summary.TextGrid.R
+++ R/summary.TextGrid.R
@@ -1,23 +1,23 @@
 # Generic functions shouldn't really need any extra documentation.
 
 summary.TextGrid <- function(x){
-  cat("Tiered?:")
+  cat("Tiered?: ")
   if (attributes(x)$tiers){
-    cat("Yes.\n")
+    cat("Yes\n")
   } else {
-    cat("No.\n")
+    cat("No\n")
   }
   cat("Number of Tiers: ")
   cat(attributes(x)$size)
   cat("\n")
-  cat("Names of Tiers: ")
-  cat(attributes(x)$names
+  cat("Names of Tiers: \n")
+  cat(attributes(x)$names)
   cat("\n")
   cat("Start Point: ")
   cat(attributes(x)$xmin)
   cat("\n")
   cat("End Point: ")
   cat(attributes(x)$xmax)
-  cat("\n")
+  #cat("\n\n")
   # Maybe add the total number of intervals to this as well.
 }
